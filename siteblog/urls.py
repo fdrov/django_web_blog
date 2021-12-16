8e2903f9d298acc2ec4f174295214b011c2fed3e
Fdrv <oldgot@gmail.com>
2021-12-16 18:12:17 +0300

    Add Django Debug Toolbar

--- siteblog/urls.py
+++ siteblog/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
