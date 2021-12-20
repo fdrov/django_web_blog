b02285e0bf59d0a9f7877700d4584a71f4fa27a9
Fdrv <oldgot@gmail.com>
2021-12-20 17:41:17 +0300

    Category page

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
-    path('category/<str:slug>/', get_category, name='category'),
+    path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
     path('post/<str:slug>/', get_post, name='post'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from django.http import HttpResponse
+from django.views.generic import ListView
 
-from .models import Post, Category, Tag
+from .models import Post, Category
 
 
 class HomeListView(ListView):
     model = Post
+    # queryset = get_list_or_404(Post, category__pk=6)
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 10
 
     # def get_context_data(self, *, object_list=None, **kwargs):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Classic blog design'
-        context['page_range'] = context['paginator'].get_elided_page_range(context['page_obj'].number, on_each_side=1, on_ends=1)
+        context['page_range'] = context['paginator'].get_elided_page_range(context['page_obj'].number, on_each_side=1,
+                                                                           on_ends=1)
+        return context
+
+
+class PostsByCategory(ListView):
+    template_name = 'blog/category.html'
+    context_object_name = 'posts_by_category'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
         return context
 
 
@@ -28,4 +44,4 @@ def get_category(request, slug):
 
 
 def get_post(request, slug):
-    return  render(request, 'blog/post.html')
+    return render(request, 'blog/post.html')
